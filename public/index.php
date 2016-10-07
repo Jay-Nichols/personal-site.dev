06eaa4ee2f68a89e75c1c57d3754358b0f2fa7bc
jay nichols <jay_nichols_3@hotmail.com>
2016-10-07 15:50:08 -0500

    adding partial files and using require

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
 
-include 'header_partial.'
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<!--  -->
+	<?php include 'partials/bootstrap_header.php' ?>
+	<?php include 'css/personal-site.css' ?>
+</head>
+<body>
+	<?php include 'partails/navbar.php' ?>
+</body>
